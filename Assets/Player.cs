a168bc1df99206a9d1200d770985101d093ea873
tatsuhiko yamamura <tatsuhiko@unity3d.com>
2014-10-03 16:16:00 +0900

    first commit

---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Transform goal;
+
+	NavMeshAgent agent;
+
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update () 
+	{
+		agent.SetDestination(goal.position);
+	}
+}
